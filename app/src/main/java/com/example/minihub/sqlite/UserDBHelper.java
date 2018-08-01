79565e32a58c78ae99af6bd72140a2945f47da84
BigImpostor <931611682@qq.com>
2018-08-01 23:42:50 +0800

    second
    Signed-off-by: BigImpostor <931611682@qq.com>

---
+++ app/src/main/java/com/example/minihub/sqlite/UserDBHelper.java
@@ -0,0 +1,26 @@
+package com.example.minihub.sqlite;
+
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserSql extends SQLiteOpenHelper{
+
+
+    public UserSql(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+    
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
